b197060789e6e132764f0fc684cf635e0c592150
alionachernenko <errorroror75@gmail.com>
2024-07-24 19:20:17 +0300

    Add basic profile logic

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/mulan17/project-user-service
 go 1.22.2
 
 require (
+	github.com/matoous/go-nanoid/v2 v2.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/rs/zerolog v1.33.0 // indirect
---
+++ internal/profile/handlers.go
@@ -0,0 +1,87 @@
+package profile
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type UserResource struct {
+	S *Storage
+}
+
+func (p *UserResource) GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	users := p.S.GetAllUsers()
+	res := map[string][]User{"users": users}
+
+	err := json.NewEncoder(w).Encode(res)
+
+	if err != nil {
+		fmt.Println("Failed to encode: ", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+}
+
+func (p *UserResource) GetUserById(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+
+	user, ok := p.S.GetUserById(id)
+
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err := json.NewEncoder(w).Encode(user)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func (p *UserResource) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var reqBody User
+
+	userId, ok := p.S.CreateUser(reqBody)
+
+	if !ok {
+		fmt.Print("Error creating user")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	reqBody.ID = userId
+
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+
+	if err != nil {
+		fmt.Println("Failed to decode: ", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
+
+func (p *UserResource) UpdateUser(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+
+	var reqBody User
+
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+
+	if err != nil {
+		fmt.Println("Failed to encode: ", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	ok := p.S.UpdateUser(id, reqBody)
+
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+}
